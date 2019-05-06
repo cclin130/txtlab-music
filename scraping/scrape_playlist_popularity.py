c3aac96fb60bc02b31687ac61480d30f75b2e163
cclin130 <cheng.lin103@gmail.com>
2019-05-05 23:13:05 -0400

    script to scrape # of followers

--- scraping/scrape_playlist_popularity.py
+++ scraping/scrape_playlist_popularity.py
@@ -7,10 +7,7 @@ Created on Sun May  5 21:49:15 2019
 import sys
 import csv
 from datetime import date
-import requests
-import json
-import base64
-from spotify_api_functions import get_access_token
+from spotify_api_functions import get_access_token, make_api_request
 
 if __name__ == '__main__':
     
@@ -22,14 +19,14 @@ if __name__ == '__main__':
     #read csv
     print('---------reading csv------------')
     table = []
-    with open(file_path, mode='r+') as f:
+    with open(file_path, encoding = 'utf-8',mode='r+') as f:
         for line in f:
             table.append(line.strip('\n').split(','))
     f.close()
     
     #modify table head to include new date
     head = table[0]
-    head.append(date.today())
+    head.append('{}_followers'.format(date.today()))
     table = table[1:]
     
     #get access token
@@ -42,12 +39,11 @@ if __name__ == '__main__':
         playlist_id = line[1]
         url = 'https://api.spotify.com/v1/playlists/{}?fields=followers'.format(playlist_id)
         
-        r = requests.get(url, headers={'Authorization': 'Bearer {}'.format(token)})
-        result = r.json()
-        
+        result = make_api_request(url, token)
+
         num_followers = result['followers']['total']
         line.append(num_followers)
-        print('{0} playlist has {1} followers'.format(line[0], num_followers)
+        print('{0} playlist has {1} followers'.format(line[0], num_followers))
 
     with open(file_path, encoding='utf-8', newline='', mode='w') as f:
         writer = csv.writer(f)
