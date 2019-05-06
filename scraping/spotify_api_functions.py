d331c20a6299ad18ea97f9646cc1881cd2235454
cclin130 <cheng.lin103@gmail.com>
2019-05-05 23:13:24 -0400

    added function for generic api call

--- scraping/spotify_api_functions.py
+++ scraping/spotify_api_functions.py
@@ -8,6 +8,9 @@ Created on Sun May  5 22:01:03 2019
 import requests
 import json
 import base64
+from ratelimit import limits, sleep_and_retry
+
+FIFTEEN_MINUTES = 900
 
 def get_access_token(CLIENT_ID, CLIENT_SECRET):
     GRANT_TYPE = 'client_credentials'
@@ -15,6 +18,13 @@ def get_access_token(CLIENT_ID, CLIENT_SECRET):
     
     r = requests.post('https://accounts.spotify.com/api/token',data=body_params, auth = (CLIENT_ID, CLIENT_SECRET))
     return r.json()['access_token']
+
+#function to make a generic spotify api call; returns a json
+@sleep_and_retry
+@limits(calls=30, period=FIFTEEN_MINUTES)
+def make_api_request(url, token):
+    r = requests.get(url, headers={'Authorization': 'Bearer {}'.format(token)})
+    return r.json()
 
 #function that pulls playlist contents given spotify playlist ID
 def pull_playlist_contents(playlist_id):
